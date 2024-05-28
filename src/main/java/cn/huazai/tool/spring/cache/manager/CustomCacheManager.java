6a26fcfb75a7cfb757c251ff10421b7f3ff0f505
YanAnHuaZai <me@huazai.cn>
2024-05-28 16:32:21 +0800

    feat: 缓存管理器使用 缓存构建工厂 获取缓存对象

--- src/main/java/cn/huazai/tool/spring/cache/manager/CustomCacheManager.java
+++ src/main/java/cn/huazai/tool/spring/cache/manager/CustomCacheManager.java
@@ -1,9 +1,10 @@
-package cn.huazai.tool.spring.cache.manager.cache;
+package cn.huazai.tool.spring.cache.manager;
 
-import cn.huazai.tool.spring.cache.manager.cache.mistake.IMistakeStrategy;
-import cn.huazai.tool.spring.cache.manager.cache.mistake.MistakeStrategyEnum;
-import cn.huazai.tool.spring.cache.manager.cache.mistake.MistakeStrategyFactory;
 import cn.huazai.tool.spring.cache.manager.core.CacheTemplate;
+import cn.huazai.tool.spring.cache.manager.handler.CacheBuilderFactory;
+import cn.huazai.tool.spring.cache.manager.mistake.IMistakeStrategy;
+import cn.huazai.tool.spring.cache.manager.mistake.MistakeStrategyEnum;
+import cn.huazai.tool.spring.cache.manager.mistake.MistakeStrategyFactory;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.Cache;
@@ -11,6 +12,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.util.Assert;
 
 import java.util.Collection;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -55,7 +57,7 @@ public class CustomCacheManager implements CacheManager {
     }
 
     public Cache getCache(String name, IMistakeStrategy mistakeStrategy) {
-        log.debug("DchCacheManager.getCache name:{}, mistakeStrategy:{}", name, mistakeStrategy);
+        log.debug("CustomCacheManager.getCache name:{}, mistakeStrategy:{}", name, mistakeStrategy);
 
         Assert.notNull(name, "Name must not be null!");
         Assert.notNull(mistakeStrategy, "MistakeStrategy must not be null!");
@@ -65,50 +67,50 @@ public class CustomCacheManager implements CacheManager {
 
     @Override
     public Collection<String> getCacheNames() {
-        log.debug("DchCacheManager.getCacheNames");
+        log.debug("CustomCacheManager.getCacheNames");
         return cacheMap.keySet();
     }
 
     /**
      * 缓存时间规则
      *
-     * @param name 缓存名称
-     *             <p>可选后缀单位</p>
-     *             <ul>
-     *                 <li>{@code ms}: 毫秒</li>
-     *                 <li>{@code s}: 秒</li>
-     *                 <li>{@code min}: 分钟</li>
-     *                 <li>{@code h}: 小时</li>
-     *                 <li>{@code day}: 天</li>
-     *                 <li>{@code month}: 月</li>
-     *                 <li>{@code year}: 年</li>
-     *             </ul>
+     * @param cacheName       缓存名称
+     *                        <p>可选后缀单位</p>
+     *                        <ul>
+     *                            <li>{@code ms}: 毫秒</li>
+     *                            <li>{@code s}: 秒</li>
+     *                            <li>{@code min}: 分钟</li>
+     *                            <li>{@code h}: 小时</li>
+     *                            <li>{@code day}: 天</li>
+     *                            <li>{@code month}: 月</li>
+     *                            <li>{@code year}: 年</li>
+     *                        </ul>
      * @param mistakeStrategy 配置错误策略
      * @return 缓存时间表达式
      */
-    private Cache getCacheByName(final String name, final IMistakeStrategy mistakeStrategy) {
-        log.debug("DchCacheManager.getCacheByName name:{}", name);
+    private Cache getCacheByName(final String cacheName, final IMistakeStrategy mistakeStrategy) {
+        log.debug("CustomCacheManager.getCacheByName cacheName:{}", cacheName);
 
-        Assert.notNull(name, "Name must not be null!");
+        Assert.notNull(cacheName, "Name must not be null!");
 
-        Cache cache = cacheMap.get(name);
+        Cache cache = cacheMap.get(cacheName);
         if (cache == null) {
             synchronized (CustomCacheManager.class) {
-                cache = cacheMap.get(name);
+                cache = cacheMap.get(cacheName);
                 if (cache == null) {
                     // 构建 缓存 对象
-                    cache = CustomCache.buildByName(cacheTemplate, name);
+                    cache = Optional.ofNullable(CacheBuilderFactory.getInstance()).map(instance -> instance.getCache(cacheTemplate, cacheName)).orElse(null);
                     if (null == cache) {
-                        // 构建为null表示缓存名称存在问题（不符合规则），使用默认缓存（1分钟）
-                        log.warn("DchCacheManager.getCacheByName '{}' 构建为null，将按照'{}'策略重试", name, mistakeStrategy);
+                        // 构建为null表示缓存名称存在问题（不符合规则）
+                        log.warn("CustomCacheManager.getCacheByName '{}' 构建为null，将按照'{}'策略重试", cacheName, mistakeStrategy);
                         if (null == mistakeStrategy) {
                             throw new IllegalStateException("MistakeStrategy must not be null");
                         }
-                        cache = mistakeStrategy.getCache(this, name);
+                        cache = mistakeStrategy.getCache(this, cacheName);
                     }
                     if (null != cache) {
-                        cacheMap.putIfAbsent(name, cache);
-                        log.info("DchCacheManager.cacheMap size:{} value:{}", cacheMap.size(), cacheMap);
+                        cacheMap.putIfAbsent(cacheName, cache);
+                        log.info("CustomCacheManager.cacheMap size:{} value:{}", cacheMap.size(), cacheMap);
                     }
                 }
             }
